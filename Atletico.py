524d1b5a1dcae99f7046d696f3546c4ba74d99f9
Sergio <you@example.com>
2025-03-25 10:18:39 +0100

    Python terminado

---
+++ Atletico.py
@@ -0,0 +1,21 @@
+import json
+Practica1 = json.load(open("Atletico.json"))
+
+# El atletico se fundo en 1903 en Madrid
+print(f"El {Practica1['club']} se fundo en {Practica1['fundacion']} en {Practica1['ciudad']}")
+
+# Juegan en el Riyad Metropolitano y esta entrenado por Diego Simeone
+print(f"Juegan en el {Practica1['estadio']} y esta entrenado por {Practica1['entrenador']}")
+
+# El atletico de madrid ha ganado la liga 11 veces
+print(f"El {Practica1['club']} ha ganado la liga {Practica1['trofeos'] ['liga']} veces")
+
+# Mi jugador favorito es Julian Alvarez
+print(f"Mi jugador favorito es {Practica1['plantilla']['Delantero'][0]}")
+
+# Jugadores
+print("Plantilla")
+for posicion in Practica1['plantilla']:
+    print(f"{posicion}")
+    for jugador in Practica1['plantilla'][posicion]:
+        print(f"{jugador}")
